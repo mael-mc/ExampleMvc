3cd4641eaf47cd11adf4ea1b3d864b5bf3304563
mael-mc <juankmoreno4@gmail.com>
2026-06-12 08:52:52 -0500

    ExampleMvc

--- src/main/java/ec/edu/espoch/examplemvc/controller/ControllerCalculadora.java
+++ src/main/java/ec/edu/espoch/examplemvc/controller/ControllerCalculadora.java
@@ -2,6 +2,7 @@
 package ec.edu.espoch.examplemvc.controller;
 
 import ec.edu.espoch.examplemvc.model.Calculadora;
+import ec.edu.espoch.examplemvc.view.View;
 
 
 public class ControllerCalculadora {
@@ -9,13 +10,24 @@ public class ControllerCalculadora {
     private Calculadora calculadora;
     private View view;
     
+    
+    public ControllerCalculadora(){
+        
+        calculadora = new Calculadora();
+    }
+    
+    private Calculadora cal = new Calculadora();
+    
+    
     public void controller(){
         
         try {
-            double numberOne;
-            double numberTwo;
+            double numberOne = 0.0;
+            double numberTwo = 0.0;
             
-            
+            calculadora.setNumUno(numberOne);
+            calculadora.setNumDos(numberTwo);
+            calculadora.sumar();
             
         } catch (Exception e) {
         }
